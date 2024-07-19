bf94c9e54ffdfa4d25664b4b253ffdea48c955f0
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-19 12:55:37 -0400

    general info now usees state for realtime input

--- src/GeneralInformation.jsx
+++ src/GeneralInformation.jsx
@@ -1,13 +1,14 @@
 import "./GeneralInformation.css"
+import { useState } from 'react'
 
-function Items({ data }) {
+function Items({ info, handleInput }) {
     return (
         <>
-            {data.map((entry) => (
+            {info.map((entry) => (
                 <div key={entry.id} className="formItem">
                     <label htmlFor={entry.id}>{entry.text}:</label>
                     <br />
-                    <input type={entry.type} id={entry.id} name={entry.id} />
+                    <input type={entry.type} id={entry.id} name={entry.id} value={entry.val} onChange={(e) => {handleInput(entry.id, e.target.value)}} />
                 </div>
             ))}
         </>
@@ -16,15 +17,25 @@ function Items({ data }) {
 
 export default function GeneralInformation() {
     const data = [
-        {text: 'Name', type: 'text', id: 'name'},
-        {text: 'Email', type: 'text', id: 'email'},
-        {text: 'Phone Number', type: 'text', id: 'phone'},
-        {text: 'Address', type: 'text', id: 'address'},
+        {text: 'Name', type: 'text', id: 'name', val:''},
+        {text: 'Email', type: 'text', id: 'email', val:''},
+        {text: 'Phone Number', type: 'text', id: 'phone', val:''},
+        {text: 'Address', type: 'text', id: 'address', val:''},
     ]
+    const [info, setInfo] = useState(data)
+
+    const handleInput = (id, val) => {
+        setInfo((prevInfo) => 
+            prevInfo.map((entry) => 
+                entry.id === id ? {...entry, val: val} : entry
+            )
+        )
+    }
+
     return (
         <fieldset>
             <legend>General Information</legend>
-            <Items data={data}/>
+            <Items info={info} handleInput={handleInput} />
         </fieldset>
     )
 }
