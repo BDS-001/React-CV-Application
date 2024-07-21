9321c5ba3160af400b38211eb73571cc2abed4a3
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-21 19:58:27 -0400

    webpage now displayes blank cv

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,29 @@ import '../styles/App.css'
 import GeneralInformation from './GeneralInformation'
 import Experiences from './Experience'
 import EducationAchievements from './Education'
-
+import CV from './Cv'
 
 function submitForm(e) {
   e.preventDefault();
-  console.log('worked')
+  document.querySelector('.cv-data-form').style.display = 'none'
+  document.querySelector('.cv-container').style.display = 'block'
 }
 
 
 function App() {
   return (
     <>
-      <form action="get" method="get">
+      <form action="get" method="get" className='cv-data-form'>
         <h1>CV Generator</h1>
         <GeneralInformation />
         <Experiences />
         <EducationAchievements />
         <button className='cv-submit' type="submit" onClick={submitForm}>Submit</button>
       </form>
+      <div className="cv-container">
+            <header className="cv-header"><h1>Generated CV</h1> <button className='cv-edit'>Edit</button></header>
+            <CV />
+        </div>
     </>
   )
 }
---
+++ src/components/Cv.jsx
@@ -0,0 +1,13 @@
+import '../styles/Cv.css'
+
+function CV() {
+    return (
+      <>
+        <div className="cv">
+            Test
+        </div>
+      </>
+    )
+  }
+  
+  export default CV
