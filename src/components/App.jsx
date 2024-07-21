55d366f743985ae251aa6655dbfbfcc66d42bea4
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-21 19:37:47 -0400

    added submit button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,22 @@ import GeneralInformation from './GeneralInformation'
 import Experiences from './Experience'
 import EducationAchievements from './Education'
 
+
+function submitForm(e) {
+  e.preventDefault();
+  console.log('worked')
+}
+
+
 function App() {
   return (
     <>
       <form action="get" method="get">
+        <h1>CV Generator</h1>
         <GeneralInformation />
         <Experiences />
         <EducationAchievements />
+        <button className='cv-submit' type="submit" onClick={submitForm}>Submit</button>
       </form>
     </>
   )
