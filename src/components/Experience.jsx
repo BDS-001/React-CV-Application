0d054315151d27f11efd976dbeff8e88edbe636c
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-25 14:32:06 -0400

    fixed inconsistencies

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,86 +9,85 @@ function GenerateExperiences({experiences, handleRemoveExperience, setState}) {
                 <div key={experience.id} id={`experience-${experience.id}`} className="experience-entry">
                     <button className='remove-button' onClick={() => HandleFadeOut(experience.id, 'experience', handleRemoveExperience)} type="button"> &times; </button>
                     <div className="experience-entry-info">
-                    <div className="form-element">
-                        <label htmlFor={`jobTitle-${experience.id}`}>Job Title:</label><br />
-                        <input
-                            type="text"
-                            id={`jobTitle-${experience.id}`}
-                            name={`jobTitle-${experience.id}`}
-                            value={experience.jobTitle}
-                            onChange={(e) =>
-                            HandleInputChange(experience.id, 'jobTitle', e.target.value, setState)
-                            }
-                        />
-                    </div>
+                        <div className="form-item">
+                            <label htmlFor={`jobTitle-${experience.id}`}>Job Title:</label><br />
+                            <input
+                                type="text"
+                                id={`jobTitle-${experience.id}`}
+                                name={`jobTitle-${experience.id}`}
+                                value={experience.jobTitle}
+                                onChange={(e) =>
+                                HandleInputChange(experience.id, 'jobTitle', e.target.value, setState)
+                                }
+                            />
+                        </div>
 
-                    <div className="form-element">
-                        <label htmlFor={`company-${experience.id}`}>Company:</label><br />
-                        <input
-                            type="text"
-                            id={`company-${experience.id}`}
-                            name={`company-${experience.id}`}
-                            value={experience.company}
-                            onChange={(e) =>
-                            HandleInputChange(experience.id, 'company', e.target.value, setState)
-                            }
-                        />
-                    </div>
+                        <div className="form-item">
+                            <label htmlFor={`company-${experience.id}`}>Company:</label><br />
+                            <input
+                                type="text"
+                                id={`company-${experience.id}`}
+                                name={`company-${experience.id}`}
+                                value={experience.company}
+                                onChange={(e) =>
+                                HandleInputChange(experience.id, 'company', e.target.value, setState)
+                                }
+                            />
+                        </div>
 
-                    <div className="form-element">
-                        <label htmlFor={`startDate-${experience.id}`}>Start Date:</label><br />
-                        <input
-                            type="date"
-                            id={`startDate-${experience.id}`}
-                            name={`startDate-${experience.id}`}
-                            value={experience.startDate}
-                            onChange={(e) =>
-                            HandleInputChange(experience.id, 'startDate', e.target.value, setState)
-                            }
-                        />
-                    </div>
+                        <div className="form-item">
+                            <label htmlFor={`startDate-${experience.id}`}>Start Date:</label><br />
+                            <input
+                                type="date"
+                                id={`startDate-${experience.id}`}
+                                name={`startDate-${experience.id}`}
+                                value={experience.startDate}
+                                onChange={(e) =>
+                                HandleInputChange(experience.id, 'startDate', e.target.value, setState)
+                                }
+                            />
+                        </div>
 
-                    <div className="form-element">
-                        <label htmlFor={`endDate-${experience.id}`}>End Date:</label><br />
-                        <input
-                            type="date"
-                            id={`endDate-${experience.id}`}
-                            name={`endDate-${experience.id}`}
-                            value={experience.endDate}
-                            onChange={(e) =>
-                            HandleInputChange(experience.id, 'endDate', e.target.value, setState)
-                            }
-                        />
-                    </div>
+                        <div className="form-item">
+                            <label htmlFor={`endDate-${experience.id}`}>End Date:</label><br />
+                            <input
+                                type="date"
+                                id={`endDate-${experience.id}`}
+                                name={`endDate-${experience.id}`}
+                                value={experience.endDate}
+                                onChange={(e) =>
+                                HandleInputChange(experience.id, 'endDate', e.target.value, setState)
+                                }
+                            />
+                        </div>
 
-                    <div className="form-element">
-                        <label htmlFor={`location-${experience.id}`}>Location:</label><br />
-                        <input
-                            type="text"
-                            id={`location-${experience.id}`}
-                            name={`location-${experience.id}`}
-                            value={experience.location}
-                            onChange={(e) =>
-                            HandleInputChange(experience.id, 'location', e.target.value, setState)
-                            }
-                        />
-                    </div>
+                        <div className="form-item">
+                            <label htmlFor={`location-${experience.id}`}>Location:</label><br />
+                            <input
+                                type="text"
+                                id={`location-${experience.id}`}
+                                name={`location-${experience.id}`}
+                                value={experience.location}
+                                onChange={(e) =>
+                                HandleInputChange(experience.id, 'location', e.target.value, setState)
+                                }
+                            />
+                        </div>
 
-                    <div className="form-element">
-                        <label htmlFor={`jobDetails-${experience.id}`}>Job Details:</label><br />
-                        <textarea
-                            id={`jobDetails-${experience.id}`}
-                            name={`jobDetails-${experience.id}`}
-                            value={experience.jobDetails}
-                            onChange={(e) =>
-                            HandleInputChange(experience.id, 'jobDetails', e.target.value, setState)
-                            }
-                        ></textarea>
-                    </div>
+                        <div className="form-item">
+                            <label htmlFor={`jobDetails-${experience.id}`}>Job Details:</label><br />
+                            <textarea
+                                id={`jobDetails-${experience.id}`}
+                                name={`jobDetails-${experience.id}`}
+                                value={experience.jobDetails}
+                                onChange={(e) =>
+                                HandleInputChange(experience.id, 'jobDetails', e.target.value, setState)
+                                }
+                            ></textarea>
+                        </div>
                     </div>
                 </div>
             ))}
-
         </>
     )
 }
--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -3,64 +3,71 @@ import { HandleInputChange } from './Handlers';
 
 function Items({ genInfo, setGenInfo }) {
     return (
-        <>
-            {genInfo.map((entry) => (
-                <div key={entry.id} className="form-item">
-                    <label htmlFor={`name-${entry.id}`}>Name:</label>
-                    <br />
-                    <input
-                        type="text"
-                        id={`name-${entry.id}`}
-                        name="name"
-                        value={entry.name}
-                        onChange={(e) => HandleInputChange(entry.id, 'name', e.target.value, setGenInfo)}
-                    />
-                    <br />
+<>
+    {genInfo.map((entry) => (
+        <div key={entry.id} className="general-info-container">
+            <div className="form-item">
+                <label htmlFor={`name-${entry.id}`}>Name:</label>
+                <br />
+                <input
+                    type="text"
+                    id={`name-${entry.id}`}
+                    name="name"
+                    value={entry.name}
+                    onChange={(e) => HandleInputChange(entry.id, 'name', e.target.value, setGenInfo)}
+                />
+            </div>
 
-                    <label htmlFor={`email-${entry.id}`}>Email:</label>
-                    <br />
-                    <input
-                        type="text"
-                        id={`email-${entry.id}`}
-                        name="email"
-                        value={entry.email}
-                        onChange={(e) => HandleInputChange(entry.id, 'email', e.target.value, setGenInfo)}
-                    />
-                    <br />
+            <div className="form-item">
+                <label htmlFor={`email-${entry.id}`}>Email:</label>
+                <br />
+                <input
+                    type="text"
+                    id={`email-${entry.id}`}
+                    name="email"
+                    value={entry.email}
+                    onChange={(e) => HandleInputChange(entry.id, 'email', e.target.value, setGenInfo)}
+                />
+            </div>
 
-                    <label htmlFor={`phone-${entry.id}`}>Phone Number:</label>
-                    <br />
-                    <input
-                        type="text"
-                        id={`phone-${entry.id}`}
-                        name="phone"
-                        value={entry.phone}
-                        onChange={(e) => HandleInputChange(entry.id, 'phone', e.target.value, setGenInfo)}
-                    />
-                    <br />
+            <div className="form-item">
+                <label htmlFor={`phone-${entry.id}`}>Phone Number:</label>
+                <br />
+                <input
+                    type="text"
+                    id={`phone-${entry.id}`}
+                    name="phone"
+                    value={entry.phone}
+                    onChange={(e) => HandleInputChange(entry.id, 'phone', e.target.value, setGenInfo)}
+                />
+            </div>
 
-                    <label htmlFor={`address-${entry.id}`}>Address:</label>
-                    <br />
-                    <input
-                        type="text"
-                        id={`address-${entry.id}`}
-                        name="address"
-                        value={entry.address}
-                        onChange={(e) => HandleInputChange(entry.id, 'address', e.target.value, setGenInfo)}
-                    />
-                    <br />
+            <div className="form-item">
+                <label htmlFor={`address-${entry.id}`}>Address:</label>
+                <br />
+                <input
+                    type="text"
+                    id={`address-${entry.id}`}
+                    name="address"
+                    value={entry.address}
+                    onChange={(e) => HandleInputChange(entry.id, 'address', e.target.value, setGenInfo)}
+                />
+            </div>
 
-                    <label htmlFor={`about-${entry.id}`}>About Me:</label>
-                    <br />
-                    <textarea
-                        id={`about-${entry.id}`}
-                        name="about"
-                        value={entry.about}
-                        onChange={(e) => HandleInputChange(entry.id, 'about', e.target.value, setGenInfo)}
-                    />
-                </div>
-            ))}
-        </>
+            <div className="form-item">
+                <label htmlFor={`about-${entry.id}`}>About Me:</label>
+                <br />
+                <textarea
+                    id={`about-${entry.id}`}
+                    name="about"
+                    value={entry.about}
+                    onChange={(e) => HandleInputChange(entry.id, 'about', e.target.value, setGenInfo)}
+                />
+            </div>
+        </div>
+    ))}
+</>
+
 
     )
 }
@@ -69,9 +76,7 @@ export default function GeneralInformation({genInfo, setGenInfo}) {
     return (
         <fieldset className="general-info">
             <legend>General Information</legend>
-            <div className="general-info-container">
-                <Items genInfo={genInfo} setGenInfo={setGenInfo} />
-            </div>
+            <Items genInfo={genInfo} setGenInfo={setGenInfo} />
         </fieldset>
     )
 }
